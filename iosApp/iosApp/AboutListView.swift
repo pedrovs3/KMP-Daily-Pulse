8d4c987b44831f168d2358c6fcaab275b88ccad9
Pedro <pedrovs3@hotmail.com>
2024-03-11 22:38:50 -0300

    feat: about screen on ios platform

---
+++ iosApp/iosApp/AboutListView.swift
@@ -0,0 +1,54 @@
+import SwiftUI
+import shared
+
+struct AboutListView: View {
+    private struct RowItem: Hashable {
+        let title: String
+        let subtitle: String
+    }
+
+    private let items: [RowItem] = {
+        let platform = Platform()
+        platform.logSystemInfo()
+
+        var result: [RowItem] = [
+        .init(
+            title: "Operating system",
+            subtitle: "\(platform.osName) \(platform.osVersion)"
+        ),
+        .init(
+            title: "Device",
+            subtitle: platform.deviceModel
+        ),
+        .init(
+            title: "Density",
+            subtitle: "@\(platform.density)x"
+        ),
+        .init(
+            title: "App version",
+            subtitle: platform.appVersion
+        )]
+        return result
+    }()
+
+    var body: some View {
+        List {
+            ForEach(items, id: \.self) { item in
+                VStack(alignment: .leading) {
+                    Text(item.title)
+                        .font(.footnote)
+                        .foregroundStyle(.secondary)
+                    Text(item.subtitle)
+                        .font(.body)
+                        .foregroundStyle(.primary)
+                }
+                .padding(.vertical, 4)
+            }
+        }
+        .navigationTitle("About")
+    }
+}
+
+#Preview {
+    AboutListView()
+}
